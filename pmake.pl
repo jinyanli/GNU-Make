b375965347b0273913b253133414eb2b0d7eb789
Jinyan Li <jinyanli.good@gmail.com>
2015-11-28 02:59:13 -0800

    parse line

--- pmake.pl
+++ pmake.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 # Jinyan Li jli134@ucsc.eud
 
+use Data::Dumper;
+use feature 'say';
 use strict;
 use warnings;
 use Getopt::Std;
@@ -17,20 +19,56 @@ getopts "dnf", \%opts;
 
 my $filename = "Makefile";
 $filename = $ARGV[0] if $opts{'f'};
-print $filename."\n";
+#print $filename."\n";
 
 my %macro_hash;
 my %target_hash;
+my %cmd_hash;
+
+my $previous_target;
+
+
 
 push @ARGV, "-" unless @ARGV;
 
 open my $file, "<$filename" or warn "$filename: $!\n" and next;
-   while (defined (my $line = <$file>)) {
+while (defined (my $line = <$file>)) {
       chomp $line;
-      printf "%6d  ", $. if $opts{'n'};
-      printf "%s\n", $line;
-   }
+      $previous_target = &parse_line($line,\%macro_hash,\%target_hash,
+                                          \%cmd_hash,$previous_target);
+      #printf "%s\n", $line;
+      print "\n";
+}
 
+sub parse_line {
+   my ($line,$macro_hash_ref,$target_hash_ref,
+                                   $cmd_hash_ref,$prev_target)=@_;
+     
+     #if the line is not comment
+     if($line !~ /^#.+/){
+
+         #put target as key and prerequisites as value in hash
+         if($line =~ /\s*(\S+)\s*:.*/ and $line !~ /\t\s*.+/) {            
+            my $target=$1;
+            print "$target : ";
+
+            if($line=~/.+:\s+(.+)/){
+               my @prerequisite=split(" ",$1);
+               print "@prerequisite";
+               my %target_hash1;
+               $target_hash_ref->{$target} = [@prerequisite];
+               #say Dumper($target_hash_ref);
+            }else{
+               $target_hash_ref->{$target}="";
+            }
+         }
+         #put corresponding command in hash
+         elseif
+
+      }
+
+}
+#say Dumper(%target_hash);
 close $file;
 
 
